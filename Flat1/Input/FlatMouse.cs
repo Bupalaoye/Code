f304712f7753367d1c4732ac3c94d52c68a63964
BUPALAOYE <3181475781@qq.com>
2023-12-03 20:57:02 +0800

    first commit

---
+++ Flat1/Input/FlatMouse.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Flat1.Graphics;
+
+namespace Flat1.Input
+{
+    public sealed class FlatMouse
+    {
+        // 懒汉单例模式
+        private static readonly Lazy<FlatMouse> Lazy = new Lazy<FlatMouse>(() => new FlatMouse());
+
+        public static FlatMouse Instance
+        {
+            get { return Lazy.Value; }
+        }
+
+
+        private MouseState prevMouseState;
+        private MouseState currMouseState;
+
+        // 这个position是相对于窗口的坐标
+        public Point WindowPosition
+        {
+            get { return this.currMouseState.Position; }
+        }
+
+        public FlatMouse()
+        {
+            this.prevMouseState = Mouse.GetState();
+            this.currMouseState = this.prevMouseState;
+        }
+
+        public void Update()
+        {
+            this.prevMouseState = this.currMouseState;
+            this.currMouseState = Mouse.GetState();
+        }
+
+        public bool IsLeftButtonDown()
+        {
+            return this.currMouseState.LeftButton == ButtonState.Pressed;
+        }
+        public bool IsRightButtonDown()
+        {
+            return this.currMouseState.RightButton == ButtonState.Pressed;
+        }
+        public bool IsMiddleButtonDown()
+        {
+            return this.currMouseState.MiddleButton == ButtonState.Pressed;
+        }
+
+
+        public bool IsLeftButtonClicked()
+        {
+            return this.currMouseState.LeftButton == ButtonState.Pressed && this.prevMouseState.LeftButton == ButtonState.Released;
+        }
+
+        public bool IsRightButtonClicked()
+        {
+            return this.currMouseState.RightButton == ButtonState.Pressed && this.prevMouseState.RightButton == ButtonState.Released;
+        }
+
+        public bool IsMiddleButtonClicked()
+        {
+            return this.currMouseState.MiddleButton == ButtonState.Pressed && this.prevMouseState.MiddleButton == ButtonState.Released;
+        }
+
+
+        public Vector2 GetScreenPosition(Screen screen)
+        {
+            Rectangle screenDestinationRectangle = screen.CalculateDestinationRectangle();
+
+            Point windowPosition = this.WindowPosition;
+
+            float sx = windowPosition.X - screenDestinationRectangle.X;
+            float sy = windowPosition.Y - screenDestinationRectangle.Y;
+
+            sx /= (float)screenDestinationRectangle.Width;
+            sy /= (float)screenDestinationRectangle.Height;
+
+            sx *= (float)screen.Width;
+            sy *= (float)screen.Height;
+            // 需要反转 因为再Screen坐标系是左下角为原点的
+            sy = (float)screen.Height - sy;
+            return new Vector2(sx, sy);
+        }
+    }
+}
---
+++ FlatAsteriods/MainShip.cs
@@ -0,0 +1,94 @@
+﻿using Flat1;
+using Flat1.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+
+
+namespace FlatAsteriods
+{
+    public class MainShip : Entity
+    {
+        private bool isRocketForce;
+        private Vector2[] rocketVertices;
+        private double randomRocketTime;
+        private double randomRocketStartTime;
+
+        public MainShip(Vector2[] vertices, Vector2 position, Color color, float density, float restitution)
+            : base(vertices, position, color, density, restitution)
+        {
+            this.isRocketForce = false;
+
+            this.rocketVertices = new Vector2[3];
+            this.rocketVertices[0] = this.vertices[3];
+            this.rocketVertices[1] = this.vertices[2];
+            this.rocketVertices[2] = new Vector2(-20f, 0f);
+
+            this.randomRocketStartTime = 0d;
+            this.randomRocketTime = 100d;
+
+            float area = MathHelper.Pi * this.Radius * this.Radius;
+            this.mass = area * density;
+            this.invMass = 1 / mass;
+        }
+
+        public override void Draw(Shapes shapes, bool displayCollisionCircle)
+        {
+            if (this.isRocketForce)
+            {
+                FlatTransform transform = new FlatTransform(this.position, this.angle, 1f);
+                shapes.DrawPolygon(this.rocketVertices, transform, 1f, Color.Yellow);
+            }
+
+            base.Draw(shapes, displayCollisionCircle);
+        }
+
+        public override void Update(GameTime gameTime, Camera camera)
+        {
+            double now = gameTime.TotalGameTime.TotalMilliseconds;
+            // 尾巴改变的时间间隔
+            if (now - this.randomRocketStartTime >= this.randomRocketTime)
+            {
+                this.randomRocketStartTime = now;
+
+                float rocketMinX = -24f;
+                float rocketMaxX = -16f;
+                float rocketMinY = -2f;
+                float rocketMaxY = 2f;
+
+                this.rocketVertices[2] = new Vector2(
+                    RandomHelper.RandomSingle(rocketMinX, rocketMaxX),
+                    RandomHelper.RandomSingle(rocketMinY, rocketMaxY));
+            }
+
+            base.Update(gameTime, camera);
+        }
+
+        public void Rotate(float amount)
+        {
+            this.angle += amount;
+
+            if (this.angle < 0)
+            {
+                this.angle += MathHelper.TwoPi;
+            }
+            if (this.angle >= MathHelper.TwoPi)
+            {
+                this.angle -= MathHelper.TwoPi;
+            }
+        }
+
+        public void ApplyForce(float amount)
+        {
+            // 其实就是单位向量
+            Vector2 forceDir = new Vector2(MathF.Cos(this.angle), MathF.Sin(this.angle));
+            this.velocity += forceDir * amount;
+
+            this.isRocketForce = true;
+        }
+
+        public void DisableRocketForce()
+        {
+            this.isRocketForce = false;
+        }
+    }
+}
